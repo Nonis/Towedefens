529f09af83ad26fcacd9363029851e241e29fccb
nonis <christian_pettersson@live.se>
2012-01-09 02:07:43 +0100

    new files

---
+++ Enemy.h
@@ -0,0 +1,14 @@
+//
+//  Enemy.h
+//  Towerdefence
+//
+//  Created by Christian Pettersson on 2012-01-09.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Towerdefence_Enemy_h
+#define Towerdefence_Enemy_h
+
+
+
+#endif
---
+++ Tower.h
@@ -0,0 +1,14 @@
+//
+//  Tower.h
+//  Towerdefence
+//
+//  Created by Christian Pettersson on 2012-01-09.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Towerdefence_Tower_h
+#define Towerdefence_Tower_h
+
+
+
+#endif
